dc248fd6a197013cd6ac8d384f4b869f6232223e
vyrosoft <41441231+vyrosoft@users.noreply.github.com>
2019-10-28 16:31:05 +0300

    Update funddisburse.php

--- funddisburse.php
+++ funddisburse.php
@@ -5,7 +5,6 @@
  <input type="hidden" name="id" value="9">
  <input type="hidden" name="phone" value="2547xxxx">
  <input type="hidden" name="amount" value="20">
-<input type="hidden" name="secret_key" value="vz1WjD2UsARqy0rFHtHsc9odMClwVdgUL5uDzxi0okk">
   <input type="hidden" name="public_key" value="ZT49onoE7pNiq1hGGtZYXBvHlsx9PveaCspBr7Ijnjh">
  <button type="submit" class="btn btn-primary" >send funds </button>
 </form>
