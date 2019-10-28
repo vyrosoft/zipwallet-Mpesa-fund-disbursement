04f45d65fa93729f646401e8a754708dc1a270d1
vyrosoft <41441231+vyrosoft@users.noreply.github.com>
2019-10-28 16:09:55 +0300

    Create funddisburse.php

---
+++ funddisburse.php
@@ -0,0 +1,11 @@
+
+
+<form method="POST" action="https://zipwalletpay.com/api/merchantmpesawithdraw" target="_blank">
+ <input type="hidden" name="username" value="zipwalletusername">
+ <input type="hidden" name="id" value="9">
+ <input type="hidden" name="phone" value="2547xxxx">
+ <input type="hidden" name="amount" value="20">
+<input type="hidden" name="secret_key" value="vz1WjD2UsARqy0rFHtHsc9odMClwVdgUL5uDzxi0okk">
+  <input type="hidden" name="public_key" value="ZT49onoE7pNiq1hGGtZYXBvHlsx9PveaCspBr7Ijnjh">
+ <button type="submit" class="btn btn-primary" >send funds </button>
+</form>
